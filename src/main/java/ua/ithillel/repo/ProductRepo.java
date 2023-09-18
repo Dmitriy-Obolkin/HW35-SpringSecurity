44a4a1b4979fd963bdc0e8e790efc3d87958c6e0
Dmytro Obolkin <obolkindm@gmail.com>
2023-09-18 23:01:23 +0300

    Added Postman collection for API endpoint testing.

--- src/main/java/ua/ithillel/repo/ProductRepo.java
+++ src/main/java/ua/ithillel/repo/ProductRepo.java
@@ -3,6 +3,6 @@ package ua.ithillel.repo;
 import ua.ithillel.model.entity.Product;
 
 public interface ProductRepo {
-    public Product findById(Integer id);
-    public Product save(Product product);
+    Product findById(Integer id);
+    Product save(Product product);
 }
