f2fc07fd9706ad16a4e31cef1980d5ce66486567
Dmytro Obolkin <obolkindm@gmail.com>
2023-09-19 23:26:57 +0300

    Add INFO-level logging to console and file; introduce AOP logging for repo methods
    - Enable INFO-level logging for all application packages and org.springframework.web.
    - Configure logs to output to both console and file in the 'logs' directory.
    - Introduce LoggingAspect to capture repository method calls, return values, and exceptions.

---
+++ src/main/java/ua/ithillel/aop/LoggingAspect.java
@@ -0,0 +1,37 @@
+package ua.ithillel.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Slf4j
+@Component
+public class LoggingAspect {
+    @Pointcut("execution(* ua.ithillel.repo..*.*(..))")
+    private void repositoryMethods() {
+
+    }
+
+    @Around("repositoryMethods()")
+    public Object aroundRepositoryMethod(ProceedingJoinPoint pjp) throws Throwable{
+        log.info("Before executing {} with params {}",
+                pjp.getSignature(), Arrays.toString(pjp.getArgs()));
+
+        Object returnValue = pjp.proceed();
+
+        log.info("After executing {} returned value: {}", pjp.getSignature(), returnValue);
+        return returnValue;
+    }
+
+    @AfterThrowing(pointcut = "repositoryMethods()", throwing = "e")
+    public void afterThrowingFromRepositoryMethod(Exception e){
+        log.error("Exception thrown: {}", e.getMessage());
+    }
+}
