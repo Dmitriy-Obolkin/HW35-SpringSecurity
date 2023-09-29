d7787b583463e64f894118b728884c1b12fc23d9
Dmytro Obolkin <obolkindm@gmail.com>
2023-09-30 00:18:56 +0300

    feat(security): Restrict POST and DELETE /products to ADMIN role
    - Added security constraints to allow only ADMIN role to execute POST and DELETE operations on /products endpoints.
    - Updated Postman collection for API endpoint testing.

--- src/main/java/ua/ithillel/spring/config/SecurityConfiguration.java
+++ src/main/java/ua/ithillel/spring/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package ua.ithillel.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -19,6 +20,8 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(reqs -> reqs
                         .requestMatchers("/ping").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/products").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/products/*").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .httpBasic(basic -> basic.authenticationEntryPoint(new AppBasicAuthenticationEntrypoint()));
 
---
+++ src/main/java/ua/ithillel/spring/model/enumeration/Role.java
@@ -0,0 +1,6 @@
+package ua.ithillel.spring.model.enumeration;
+
+public enum Role {
+    USER,
+    ADMIN
+}
